900966be11c19a9b33375d24ad033bb8971a73e9
mohammad.barek <>
2023-01-03 14:42:29 +0200

    initial setup

---
+++ Podfile
@@ -0,0 +1,14 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'SpaceX' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for SpaceX
+
+pod 'lottie-ios', '~> 3.1.6'
+pod 'Kingfisher', '~> 5.13'
+pod 'SwiftLint', '~> 0.39.2'
+
+end
---
+++ SpaceX/Application/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  SpaceX
+//
+//  Created by MohammadBarek on 2023-01-03.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window:UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
